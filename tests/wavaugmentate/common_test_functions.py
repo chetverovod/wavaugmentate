06d9a3b5b2b7ac47358d89fa814bf2abc16369f4
Igor Plastov <chetverovod@gmail.com>
2024-10-02 13:37:41 +0300

    Refactoring paths calculation.

--- tests/wavaugmentate/common_test_functions.py
+++ tests/wavaugmentate/common_test_functions.py
@@ -3,7 +3,7 @@ import os
 import sys
 import mcs as ms
 import wavaug as wau
-# sys.path.append('/home/igor/Plastov/wavaugmentate/src/wavaugmentate')
+
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(
     os.path.dirname(__file__))), "/src/wavaugmentate"))
 
@@ -15,11 +15,15 @@ frm_list = [60, 140, 230, 300]  # Speech formants list.
 
 # Output files names.
 TESTS_DIR = "./tests/wavaugmentate/"
-TEST_SOUND_1_FILE = TESTS_DIR + "test_sounds/test_sound_1.wav"
-TEST_SOUND_1_AC_FILE = TESTS_DIR + "test_sounds/test_sound_1_ac.wav"
-TEST_SOUND_1_DELAY_FILE = TESTS_DIR + "test_sounds/test_sound_1_delay.wav"
-TEST_SOUND_1_ECHO_FILE = TESTS_DIR + "test_sounds/test_sound_1_echo.wav"
-TEST_SOUND_1_NOISE_FILE = TESTS_DIR + "test_sounds/test_sound_1_noise.wav"
+TEST_SOUND_1_FILE = os.path.join(TESTS_DIR, "test_sounds/test_sound_1.wav")
+TEST_SOUND_1_AC_FILE = os.path.join(TESTS_DIR,
+                                    "test_sounds/test_sound_1_ac.wav")
+TEST_SOUND_1_DELAY_FILE = os.path.join(TESTS_DIR,
+                                       "test_sounds/test_sound_1_delay.wav")
+TEST_SOUND_1_ECHO_FILE = os.path.join(TESTS_DIR,
+                                      "test_sounds/test_sound_1_echo.wav")
+TEST_SOUND_1_NOISE_FILE = os.path.join(TESTS_DIR,
+                                       "test_sounds/test_sound_1_noise.wav")
 
 OUTPUTWAV_DIR = TESTS_DIR + "outputwav/"
 OUTPUT_FILE = OUTPUTWAV_DIR + "out.wav"
