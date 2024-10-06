fc4b995d36acadc2af3a2280e2d0fc849e4d7c36
Igor Plastov <chetverovod@gmail.com>
2024-10-06 09:58:20 +0300

    Build docs.

--- prep_publication.sh
+++ prep_publication.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
+#
 cd docs
 make clean
 make html
 cd ..
 git commit -am "Build docs." 
+python3 -m build
+git commit -am "Build package." 
+git push
+
+# Private file with tokens
+shell tokens.sh
+
+# Upload to TestPyPi
+python3 -m twine upload --repository testpypi dist/* --username __token__ --password $TEST_PYPI_TOKEN
